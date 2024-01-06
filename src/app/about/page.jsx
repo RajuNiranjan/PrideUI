860ef7585c39321fe55433e5ac6b8ed91d9efc48
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-01-06 10:01:59 +0530

    created about page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,7 +2,28 @@ import SampleLayout from "@/components/sampleLayout/SampleLayout";
 import React from "react";
 
 const page = () => {
-  return <div>about page</div>;
+  return (
+    <div>
+      <SampleLayout
+        mainHeading="About Agency"
+        heading="We create digital ideas that are bigger, bolder, brave and better"
+        desc=" Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora,
+          sapiente.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora,
+          sapiente.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora,
+          sapiente."
+        rightImg="/about.png"
+        img="/brands.png"
+        btn1="Learn more"
+        btn2="Contact"
+        count1="10K+"
+        count2="231K+"
+        count3="5K+"
+        countHeading1="year of exoeriences"
+        countHeading2="people reached"
+        countHeading3="service and plugins"
+      />
+    </div>
+  );
 };
 
 export default page;
