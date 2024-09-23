f7aa57968b81d9d69fd07a6fb88189dda678665c
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-09-23 14:16:56 +0530

    initialized next.js setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,8 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <title>Categories</title>
-      <body>
+      <body className="antialiased">
         <Main />
         <NextScript />
       </body>
